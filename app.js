220b557b4fd71597ce1128018ee5f6ccac9ad3d1
Paras Anand <qtparas@gmail.com>
2018-11-01 10:59:52 +0530

    Login part done

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 require('chromedriver');
 var webdriver = require('selenium-webdriver');
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
 
 (async function main() {
-    let driver = new webdriver.Builder()
+  let driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
   try {
-    await driver.get('http://182.76.79.200/Empower/Login.aspx');
-    //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('Vserv Timesheet Login'), 1000);
-    await driver.findElement(By.id('txtUserID')).sendKeys('VBS1952');
-    await driver.findElement(By.id('txtPassword')).sendKeys('Password1');
-    await driver.findElement(By.id('btnUserLogin')).click();
-    
-    
-    
+
+    //Opens and login into timesheet
+    await login(driver);
+
   } finally {
-    //await driver.quit();
+    await driver.sleep(5000);
+    await driver.quit();
   }
 })();
+
+
+async function login(driver) {
+  await driver.get('http://182.76.79.200/Empower/Login.aspx');
+  //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+  await driver.wait(until.titleIs('Vserv Timesheet Login'), 1000);
+  await driver.findElement(By.id('txtUserID')).sendKeys('VBS1952');
+  await driver.findElement(By.id('txtPassword')).sendKeys('Password1');
+  await driver.findElement(By.id('btnUserLogin')).click();
+}
+
